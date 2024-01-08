4d4fcbf3ab5a9b3fdbb206045b306408a30373e1
EdamAme <121654029+EdamAme-x@users.noreply.github.com>
2024-01-08 19:12:46 +0900

    add

--- components/hero/index.tsx
+++ components/hero/index.tsx
@@ -2,8 +2,8 @@ import { Hero3D } from "./3d";
 
 export function Hero() {
     return <div className="w-full h-[75vh] p-4 flex items-center">
-        <h1 className="flex flex-col w-1/2">
-            <span className="text-4xl">Welcome to</span>
+        <h1 className="flex flex-col w-1/2 pl-5">
+            <span className="text-4xl font-bold">Welcome to</span>
             <span className="text-6xl font-bold text-gradient">Amex Portfolio</span>
         </h1>
         <Hero3D />
