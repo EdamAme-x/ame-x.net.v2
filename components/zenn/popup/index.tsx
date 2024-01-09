a6cfd0074350c61b752fda1544948065ad9479ad
EdamAme <121654029+EdamAme-x@users.noreply.github.com>
2024-01-09 23:07:21 +0900

    add

--- components/zenn/popup/index.tsx
+++ components/zenn/popup/index.tsx
@@ -2,10 +2,26 @@ import type { Article } from "@/backend/controller/zenn/getArticles";
 
 export function ZennPopup(
     {
-        article,
+        getArticle,
         closeCallback
     }: {
-        article: Article,
+        getArticle: () => Promise<Article>,
         closeCallback: Function
     }
-) {}
+) {} // logic
+
+export function ZennPopupFallback() {
+    return <></>
+}
+
+export async function ZennPopupContent({
+    article,
+    getArticle
+}: {
+    article: Article,
+    getArticle: () => Promise<Article>
+}) {
+    const result = await getArticle();
+
+    return <>{result.id}</>
+} // ui
